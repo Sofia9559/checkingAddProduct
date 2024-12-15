5089f29d274295bb6999da89b3179dade9c8c6c2
Sonya <trsofia95@mail.ru>
2024-12-15 22:16:35 +0300

    cucumber wp

--- src/test/java/tests/dataBase/DataBaseTest.java
+++ src/test/java/tests/dataBase/DataBaseTest.java
@@ -1,7 +1,7 @@
 package tests.dataBase;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.sql.*;
 
--- src/test/java/tests/testRunner/TestRunner.java
+++ src/test/java/tests/testRunner/TestRunner.java
@@ -1,10 +1,18 @@
 package tests.testRunner;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.ConfigurationParameters;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = "tests.steps.ui_steps", plugin = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@ConfigurationParameters({
+        @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features"),
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "tests.steps.ui_steps"),
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
+})
 public class TestRunner {
 }
